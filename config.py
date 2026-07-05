0952fab7a39ea2a170aae0e2b3bb674d1de25073
anuj9897942014 <anujksharma1995@gmail.com>
2026-07-06 04:42:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
+# Telegram Bot Token
 BOT_TOKEN = "8886719368:AAEg-ux8qQA0n4HS2-fDfWV4eLI6_qWMtkA"
 
-# Channel Username
-CHANNEL_USERNAME = "Daily Lucky Draw Result"
-
-# India Time Zone
-TIMEZONE = "Asia/Kolkata"
-
-BOT_TOKEN = "8886719368:AAEg-ux8qQA0n4HS2-fDfWV4eLI6_qWMtkA"
-
+# Public Channel Username (Bot must be Admin)
 CHANNEL_ID = "Daily Lucky Draw Result"
+
+# Timezone
+TIMEZONE = "Asia/Kolkata"
