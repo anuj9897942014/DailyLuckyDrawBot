053a64a7a0829240a6d0fee090884ee2e806c8d7
anuj9897942014 <anujksharma1995@gmail.com>
2026-07-06 03:57:17 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conn = sqlite3.connect("luckydraw.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS participants (
+    user_id INTEGER,
+    username TEXT
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS winners (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    win_time TEXT
+)
+""")
+
+conn.commit()
