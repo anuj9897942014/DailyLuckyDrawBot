6299fc73e5d2c5b554bad617985848f7d3a573a9
anuj9897942014 <anujksharma1995@gmail.com>
2026-07-06 04:02:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ CHANNEL_USERNAME = "Daily Lucky Draw Result"
 
 # India Time Zone
 TIMEZONE = "Asia/Kolkata"
+
+BOT_TOKEN = "8886719368:AAEg-ux8qQA0n4HS2-fDfWV4eLI6_qWMtkA"
+
+CHANNEL_ID = "Daily Lucky Draw Result"
